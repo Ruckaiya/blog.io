a2e8e22405c37164163e778e571a2c0beb4bda23
Ruckaiya <50162592+Ruckaiya@users.noreply.github.com>
2020-07-24 00:33:25 +0600

    Add files via upload

---
+++ assets/js/questions.js
@@ -0,0 +1,70 @@
+const quizQuestions = [{
+        question: "What is my full name?",
+        choices: ["Esterling Accime", "Esterling", "Accime", "Sadrack Peterson"],
+        correctAnswer: "Esterling Accime. You can read about it more on- link provided "
+    },
+
+    {
+        question: "What is your favorite sports?",
+        choices: ["Soccer", "Baseball", "Footbal", "Swimming"],
+        correctAnswer: "Soccer"
+    },
+
+    {
+        question: "What is your favorite sports?",
+        choices: ["Soccer", "Baseball", "Footbal", "Swimming"],
+        correctAnswer: "Soccer"
+    },
+
+    {
+        question: "What is your favorite sports?",
+        choices: ["Soccer", "Baseball", "Footbal", "Swimming"],
+        correctAnswer: "Soccer"
+    },
+    {
+        question: "What is your favorite sports?",
+        choices: ["Soccer", "Baseball", "Footbal", "Swimming"],
+        correctAnswer: "Soccer"
+    },
+    {
+        question: "What is your favorite sports?",
+        choices: ["Soccer", "Baseball", "Footbal", "Swimming"],
+        correctAnswer: "Soccer"
+    },
+    {
+        question: "What is your favorite sports?",
+        choices: ["Soccer", "Baseball", "Footbal", "Swimming"],
+        correctAnswer: "Soccer"
+    },
+    {
+        question: "What is your favorite sports?",
+        choices: ["Soccer", "Baseball", "Footbal", "Swimming"],
+        correctAnswer: "Soccer"
+    },
+    {
+        question: "What is your favorite sports?",
+        choices: ["Soccer", "Baseball", "Footbal", "Swimming"],
+        correctAnswer: "Soccer"
+    },
+
+
+    {
+        question: "What town did you live at?",
+        choices: ["Fort Liberte", "Ouanaminthe", "Petion Ville", "Delmas"],
+        correctAnswer: "Ouanaminthe"
+    }
+];
+
+const funImages = [
+    './assets/images/happy fun.gif',
+    './assets/images/hellYeah.gif',
+    './assets/images/winning.gif',
+    './assets/images/yeah.gif',
+    './assets/images/you did it.gif'
+];
+
+const sadImages = [
+    './assets/images/danm.gif',
+    './assets/images/oops.gif',
+    './assets/images/pretty bad.gif'
+];
